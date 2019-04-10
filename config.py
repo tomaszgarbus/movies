fd5e3ef1f8d9cbb97fbfabee184654fd9edf9408
Tomasz Garbus <tomasz.garbus1@gmail.com>
2019-04-10 15:27:26 +0200

    OMDB constants in config

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 # My API key for omdbapi.com.
 OMDB_API_KEY = '7ff171ab'
+
+# Relative path to data directory.
+DATA_DIR = 'data'
+
+# Path to cached json responses from omdbapi.com.
+OMDB_JSONS_DIR = DATA_DIR + '/' + 'omdb_jsons'
