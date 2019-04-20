b00d8131d160b21730f2e1ee8a709b51aa24b68c
Tomasz Garbus <tgarbus@chromium.org>
2019-04-20 14:36:50 +0200

    Cached jsons

--- tmp.py
+++ tmp.py
@@ -4,9 +4,13 @@ from dbpedia_helper import DBPediaHelper
 from omdb_download import get_and_cache_movie_json
 from visualize_json import VisualizeJson
 from wiki_articles_download import tokens_list_to_context_json, tokenize_article_text, filter_no_letter_or_digit,\
-    download_article_or_load_from_cache, cut_by_citations
+    download_article_or_load_from_cache, cut_by_citations, number_heuristic
+from config import PRETRAINED_W2V_PATH
 
 if __name__ == '__main__':
+    model = gensim.models.KeyedVectors.load_word2vec_format(PRETRAINED_W2V_PATH, binary=True)
+    json_viz = VisualizeJson(model)
+
     helper = DBPediaHelper()
     dbpedia_json = helper.load_resourse_as_flat_json("Star_Wars_(film)")
     print(dbpedia_json)
@@ -16,22 +20,11 @@ if __name__ == '__main__':
     wikipedia_text = cut_by_citations(wikipedia_text)
     print(len(wikipedia_text))
 
-    def number_heuristic(s: str) -> bool:
-        """ True iff |s| should be treated as a number. """
-        allowed_chars = '0123456789,.%/$E'
-        any_digit = False
-        for c in s:
-            if c.isdigit():
-                any_digit = True
-            if c not in allowed_chars:
-                return False
-        return any_digit
     wikipedia_tokenized = filter_no_letter_or_digit(tokenize_article_text(wikipedia_text))
     print(len(wikipedia_tokenized))
     wikipedia_json = tokens_list_to_context_json(wikipedia_tokenized, include_if=number_heuristic)
     print(wikipedia_json)
 
-    model = gensim.models.KeyedVectors.load_word2vec_format('./data/GoogleNews-vectors-negative300.bin', binary=True)
-    json_viz = VisualizeJson(model)
+
     # json_viz.visualize_many([omdb_json, dbpedia_json, wikipedia_json])
     json_viz.visualize_many([omdb_json, wikipedia_json], show_context=False)
