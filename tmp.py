1b6249b37adfdc0aa0dade83ecaa64fa26c39860
Tomasz Garbus <tgarbus@chromium.org>
2019-04-20 19:21:52 +0200

    Added a function with multiple window sizes to tmp, docstringed the functions

--- tmp.py
+++ tmp.py
@@ -1,5 +1,6 @@
 import gensim
 import csv
+from typing import List
 
 from config import PRETRAINED_W2V_PATH, MOVIES_TO_FETCH_PATH
 from omdb_download import get_and_cache_movie_json, preprocess_movie_json
@@ -9,8 +10,14 @@ from wiki_articles_download import tokens_list_to_context_json, download_article
 from utils import number_heuristic
 
 
-def compare_omdb_with_wiki(json_viz: VisualizeJson, wiki_window_size=10):
-    """ Some experiments with OMDB Api and Wikipedia articles. No interesting results so far. """
+def compare_omdb_with_wiki(json_viz: VisualizeJson, wiki_window_size=10) -> None:
+    """
+    Compares OMDb jsons with jsonized Wikipedia articles, using t-SNE visualizations of context embeddings as well as
+    finding closest contexts to each number found in |json_viz|.
+
+    :param json_viz: An instance of VisualizeJson.
+    :param wiki_window_size: Radius of a context window for Wikipedia.  # TODO: rename variable to wiki_window_radius
+    """
     with open(MOVIES_TO_FETCH_PATH, 'r') as movies_csv:
         reader = csv.reader(movies_csv, delimiter=',', )
         csv_rows = [row for row in reader][1:]
@@ -42,7 +49,58 @@ def compare_omdb_with_wiki(json_viz: VisualizeJson, wiki_window_size=10):
                                 limit_per_json=50)
 
 
-def compare_wiki_articles(json_viz: VisualizeJson, wiki_window_size=10):
+def compare_omdb_with_wiki_multi_window_sizes(json_viz: VisualizeJson,  # TODO: make the default argument immutable
+                                              wiki_window_sizes: List[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) -> None:
+    """
+    Compares OMDb jsons with jsonized Wikipedia articles, using t-SNE visualizations of context embeddings as well as
+    finding closest contexts to each number found in |json_viz|.
+    The jsonization of Wikipedia article is performed multiple times (ones for each window size).
+
+    :param json_viz: An instance of VisualizeJson.
+    :param wiki_window_sizes: Radiuses of context windows for Wikipedia.
+    """
+    with open(MOVIES_TO_FETCH_PATH, 'r') as movies_csv:
+        reader = csv.reader(movies_csv, delimiter=',', )
+        csv_rows = [row for row in reader][1:]
+
+    for row in csv_rows:
+        print("Row id: " + str(row[0]))
+        omdb_query = row[1]
+        pedia_resource = row[2]
+
+        omdb_json = preprocess_movie_json(get_and_cache_movie_json(omdb_query))
+        wikipedia_text = download_article_or_load_from_cache(pedia_resource)
+        wikipedia_text = preprocess_wiki_article(wikipedia_text)
+        wikipedia_tokenized = tokenize_article_text(wikipedia_text)
+        # tokens_lim = 1500
+        wikipedia_json = {}
+        for window_size in wiki_window_sizes:
+            tmp_wiki_json = tokens_list_to_context_json(wikipedia_tokenized,
+                                                        window_size=window_size,
+                                                        include_if=number_heuristic)
+            wikipedia_json = {**wikipedia_json, **tmp_wiki_json}
+
+        omdb_pairs = json_viz.get_number_context_pairs(omdb_json)
+        wiki_pairs = json_viz.get_number_context_pairs(wikipedia_json)
+        for (num, convec, conraw) in omdb_pairs:
+            closest_wiki = json_viz.k_closest_contexts(convec, wiki_pairs, k=15)
+            print("number:", num, "context:", conraw)
+            print('\n'.join(list(map(lambda a: str((a[1], a[0][0], a[0][2])), closest_wiki))))
+            print()
+
+        json_viz.visualize_many([omdb_json, wikipedia_json],
+                                show_context=False,
+                                limit_per_json=50)
+
+
+def compare_wiki_articles(json_viz: VisualizeJson, wiki_window_size=10) -> None:
+    """
+    Visualizes together numeric values from multiple articles from Wikipedia. For each value from the first article,
+    shows the closest contexts from all other articles.
+
+    :param json_viz: An instance of VisualizeJson.
+    :param wiki_window_size: Radius of the context window used when jsonizing Wikipedia articles.
+    """
     with open(MOVIES_TO_FETCH_PATH, 'r') as movies_csv:
         reader = csv.reader(movies_csv, delimiter=',', )
         csv_rows = [row for row in reader][1:]
@@ -65,6 +123,7 @@ def compare_wiki_articles(json_viz: VisualizeJson, wiki_window_size=10):
         all_wiki_pairs.append(wiki_pairs)
 
     json_viz.visualize_many(wiki_jsons, show_context=False)
+
     for pair in all_wiki_pairs[0]:
         print(pair[0], "::", pair[2])
         for j in range(1, 6):
