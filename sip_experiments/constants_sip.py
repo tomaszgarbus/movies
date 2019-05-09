f5eaa0fdeff7103b5e3f346a9ef7732cd4a681b4
Tomasz Garbus <tgarbus@chromium.org>
2019-05-09 11:41:46 +0200

    wip

--- sip_experiments/constants_sip.py
+++ sip_experiments/constants_sip.py
@@ -7,3 +7,7 @@ import os
 # Path to the sample of size 1000.
 # SAMPLE_1000_PATH = os.path.join(SIP_PATH, 'sample1000')
 SAMPLE_1000_PATH = 'data/sample1000'
+
+# The full tax name to filter the documents by. Only documents
+# referring to this tax should be considered.
+TAX_NAME = 'Ustawa o podatku od towarów i usług'
