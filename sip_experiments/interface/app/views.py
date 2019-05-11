1eb9921118dff2a7c99abe0b5d193104732439ac
Tomasz Garbus <tgarbus@chromium.org>
2019-05-11 13:37:02 +0200

    Proper bootstrap

--- sip_experiments/interface/app/views.py
+++ sip_experiments/interface/app/views.py
@@ -3,24 +3,37 @@ from django.http import HttpResponse
 import os
 from app.utils import load_top_5_similar, open_raw_html, get_keywords_heur, get_subject_heur
 from app.constants import SAMPLE_1000_DIR, NA_STR
+from typing import Dict
 
 
 def index(request):
     return HttpResponse("Hello, world.")
 
 
-def show(request, doc_name):
+def build_doc_description(doc_name: str) -> Dict[str, str]:
+    """
+    Builds a dictionary describing a document, given the document name.
+
+    :param doc_name: Document name. Must match pattern 'c\d\d\d\d\d\d', where '\d' is a digit.
+    :return: A dictionary.
+    """
     sims = load_top_5_similar(doc_name)
     raw_html = open_raw_html(doc_name)
     keywords = get_keywords_heur(raw_html)
     subject = get_subject_heur(raw_html)
     subject = subject if subject is not None else NA_STR
-    return render(request, 'show.html', context={
+    return {
         'sims': sims,
         'subject': subject,
         'keywords': keywords,
         'doc_name': doc_name,
         'doc_src': 'sample1000/' + doc_name + '.html'
+    }
+
+
+def show(request, doc_name):
+    return render(request, 'show.html', context={
+        'desc': build_doc_description(doc_name=doc_name)
     })
 
 
@@ -36,8 +49,6 @@ def listall(request):
 
 def compare(request, doc_name1: str, doc_name2: str):
     return render(request, 'compare.html', context={
-        'doc1_name': doc_name1,
-        'doc2_name': doc_name2,
-        'doc1_src': 'sample1000/' + doc_name1 + '.html',
-        'doc2_src': 'sample1000/' + doc_name2 + '.html',
+        'desc1': build_doc_description(doc_name1),
+        'desc2': build_doc_description(doc_name2)
     })
